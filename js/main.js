814852c9ca5255c0a863e02c2f3c125afe10b0e5
Pachi Parra <44537285+pachicodes@users.noreply.github.com>
2023-02-23 18:38:54 +0000

    basics

---
+++ js/main.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("novoItem");
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value);
+});
+
+function criaElemento(nome, quantidade){
+    console.log(nome);
+    console.log(quantidade);
+};
