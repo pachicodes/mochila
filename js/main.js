ea46205bd9ff12106524306fe2431bfb28acad3b
pachicodes <pachicodes@gmail.com>
2023-02-23 17:18:43 -0300

    lista ok

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("novoItem");
+const lista = document.getElementById('lista');
 
 form.addEventListener("submit", (evento) => {
     evento.preventDefault();
@@ -8,4 +9,15 @@ form.addEventListener("submit", (evento) => {
 function criaElemento(nome, quantidade){
     console.log(nome);
     console.log(quantidade);
+
+    const novoItem = document.createElement('li');
+    novoItem.classList.add('item');
+
+    const numeroItem = document.createElement('strong');
+    numeroItem.innerHTML = quantidade;
+
+    novoItem.appendChild(numeroItem);
+    novoItem.innerHTML += nome;    
+   
+    lista.appendChild(novoItem);
 };
